3abcd901cd3961aa642ee0c80e3432c24cb7bf01
gmazion <gmazion@purdue.edu>
2023-09-08 22:13:01 -0400

    run install testing/logging
    Implementation of run install testing and logging.

---
+++ runInstall.test.ts
@@ -0,0 +1,18 @@
+
+// runInstall.test.ts
+
+const { execSync } = require('child_process');
+
+describe('Logging and Testing', () => {
+  it('should log installation messages', () => {
+    const command = './run install';
+    try {
+      const output = execSync(command).toString();
+      expect(output).toContain('Installing');
+      expect(output).toContain('installed successfully.');
+    } catch (error) {
+      // If the script fails or throws an error, fail the test
+      throw error;
+    }
+  });
+});
